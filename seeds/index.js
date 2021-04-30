08944b4693ba7e10e403d95a2d961ab28bf98e87
Ayush Mittal <ayushmittal02@gmail.com>
2021-05-01 00:07:58 +0530

    add author id

--- seeds/index.js
+++ seeds/index.js
@@ -28,7 +28,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
-            author: '608c2298b18fc85c776bec54',
+            author: '608c4e01ce250f0015214aff',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
